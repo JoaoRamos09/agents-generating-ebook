b1202a3dc0f935f15a77aac092ed7a1b0ed3dee6
Joao Ramos <joaorsramos@hotmail.com>
2024-12-12 21:23:45 -0300

    add examples env

--- graph/models/state/State.py
+++ graph/models/state/State.py
@@ -1,6 +1,7 @@
 from typing import TypedDict
 from graph.models.responses.ResumeChapter import ResumeChapterResponse
 from graph.models.responses.SearchChapter import SearchChapterResponse
+
 class State(TypedDict):
     title: str
     input_user: str
